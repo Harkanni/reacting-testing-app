49bc1916d26018b19d09fe7a0b6b42acdda22a30
harkanni <harkanzyempire001@gmail.com>
2024-01-11 21:58:54 +0100

    init commit

--- src/Userform.jsx
+++ src/Userform.jsx
@@ -1,25 +1,37 @@
 import React, { useState } from 'react';
 
 const Userform = ({ onAddUser }) => {
-   const [state, setState] = useState({ name: '', email: '' });
-   const handleSubmit = (event) => {
-      event.preventDefault()
+  const [state, setState] = useState({ name: '', email: '' });
+  const handleSubmit = (event) => {
+    event.preventDefault();
 
-      onAddUser(state)
-   }
+    onAddUser(state);
+  };
 
-   const handleChange = (event) => {
-      setState({...state, [event.target.name]: event.target.value})
-   }
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.value });
+  };
   return (
     <form onSubmit={handleSubmit}>
       <div>
-        <label htmlFor=''>Name</label>
-        <input type='text' name='name' value={state.name} onChange={handleChange}  />
+        <label htmlFor='name'>Name</label>
+        <input
+          id='name'
+          type='text'
+          name='name'
+          value={state.name}
+          onChange={handleChange}
+        />
       </div>
       <div>
-        <label htmlFor=''>Email</label>
-        <input type='email' name='email' value={state.email} onChange={handleChange} />
+        <label htmlFor='email'>Email</label>
+        <input
+          id='email'
+          type='email'
+          name='email'
+          value={state.email}
+          onChange={handleChange}
+        />
       </div>
       <button>Add User</button>
     </form>
--- src/Userform.test.js
+++ src/Userform.test.js
@@ -6,11 +6,47 @@ test('Shows two inputs and a button', async () => {
   // RENDER THE COMPONENT
   render(<Userform />);
 
-  // MANIPULATE THE COMPONENT
+  // MANIPULATE THE COMPONENT OR FIND AN ELEMENT
   const inputs = screen.getAllByRole('textbox');
   const buttons = screen.getByRole('button', { name: /Add User/i });
 
   // CREATE AN ASSERTION
   expect(inputs).toHaveLength(2);
-  expect(buttons).toBeInTheDocument()
+  expect(buttons).toBeInTheDocument();
+});
+
+test('Calls onUserAdd when form is submitted', async () => {
+  // NOT THE BEST IMPLEMENTATION
+  // const argList = []
+  // const callback = (...args) => {
+  //    argList.push(args)
+  //    console.log('argList: ', argList)
+  // }
+
+  const mock = jest.fn();
+
+  // RENDER COMPONENT
+  render(<Userform onAddUser={mock} />);
+
+  // FIND THE TWO INPUTS
+  const nameInput = screen.getByRole('textbox', { name: /name/i });
+  const emailInput = screen.getByRole('textbox', { name: /email/i });
+
+  // SIMULATE TYPING IN NAME AND EMAIL FIELD
+  user.click(nameInput);
+  user.keyboard('Jane Doe');
+
+  user.click(emailInput);
+  user.keyboard('janedoe@gmail.com');
+
+  // FIND THE BUTTON AND SIMULATE CLICKING
+  const button = screen.getByRole('button');
+  user.click(button);
+
+  // ASSERTION TO MAKE SURE 'onUserAdd' GETS CALLED WITH EMAIL AND NAME
+  expect(mock).toHaveBeenCalled();
+  expect(mock).toHaveBeenCalledWith({
+    name: 'Jane Doe',
+    email: 'janedoe@gmail.com'
+  });
 });
