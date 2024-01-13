cf32b992678b1e6a6d37ac27922bc8cd996a5fca
harkanni <harkanzyempire001@gmail.com>
2024-01-13 21:04:40 +0100

    jest queries

---
+++ src/QueryTest.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { getByRole, render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
+
+const Component = () => {
+  return (
+    <div>
+      <header>Hello Akanni from JEST</header>
+      <h3>React Test</h3>
+
+      <label htmlFor='name'>name</label>
+      <input type='text' id='name' />
+
+      <label htmlFor='email'>email</label>
+      <input type='email' id='email' placeholder='enter your email' />
+
+      <label htmlFor='count'>Count</label>
+      <input type='number' id='count' />
+
+      <button>Submit</button>
+
+      <ul>
+        <li>Admin</li>
+        <li>Home</li>
+        <li>Users</li>
+        <li>Contact Us</li>
+      </ul>
+
+      <img src='./img' alt='image description' />
+
+      <footer />
+    </div>
+  );
+};
+
+test('Test query functions', () => {
+   render(<Component />)
+
+   const header = screen.getByRole('banner')
+   const h3 = screen.getByText(/REACT TEST/i)
+   const nameInput = screen.getByRole('textbox', { name: 'name'})
+
+})
+
