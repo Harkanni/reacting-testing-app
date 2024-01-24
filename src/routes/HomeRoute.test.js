f6d83c6c5d6e3a2d6c096df8caa5995b71d5d0cb
harkanni <harkanzyempire001@gmail.com>
2024-01-24 14:11:52 +0100

    hometest

--- src/routes/HomeRoute.test.js
+++ src/routes/HomeRoute.test.js
@@ -7,6 +7,41 @@ import HomeRoute from './HomeRoute';
 import { rest } from 'msw';
 import { setupServer } from 'msw/lib/node';
 
+const handlers = [
+   rest.get('/api/repositories', (req, res, ctx) => {
+      const query = req.url.searchParams.get('q')
+      console.log(query)
+
+      return res(
+         ctx.json({
+            items: [
+               { id: 1, full_name: 'full name!!'},
+               { id: 2, full_name: 'other name!!'}
+            ]
+         })
+      )
+   })
+]
+
+const server = setupServer(...handlers) 
+
+beforeAll(() => {
+   server.listen();
+})
+afterEach(() => {
+   server.resetHandlers()
+})
+afterAll(() => {
+   server.close()
+})
+
+test('renders two links for each language', () => {
+   render(
+      <MemoryRouter>
+         <HomeRoute />
+      </MemoryRouter>
+   )
+})
  
 
 
